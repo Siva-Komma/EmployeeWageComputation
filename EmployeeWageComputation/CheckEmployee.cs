a6e2806c8e6bf004ed0c96768157f2f6853d8b20
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-09 18:10:39 +0530

    Calculating Employee wages using Switch Case

--- EmployeeWageComputation/CheckEmployee.cs
+++ EmployeeWageComputation/CheckEmployee.cs
@@ -18,18 +18,30 @@ namespace EmployeeWageComputation
 
             Random random = new Random();
             int empCheck = random.Next(3);
-            if (empCheck == Emp_Present)
+            switch(empCheck)
             {
-                empHrs = 8;
+                case 0:
+                    empHrs = 8;
+                    break;
+                case 1:
+                    empHrs = 4;
+                    break;
+                case 2:
+                    empHrs = 0;
+                    break;
             }
-            else if(empCheck==Is_Part_Time)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+            //if (empCheck == Emp_Present)
+            //{
+            //    empHrs = 8;
+            //}
+            //else if(empCheck==Is_Part_Time)
+            //{
+            //    empHrs = 4;
+            //}
+            //else
+            //{
+            //    empHrs = 0;
+            //}
             EmpWage = empHrs * Emp_Rate_Per_Hour;
             Console.WriteLine("Emp Wage: " + EmpWage);
         }
