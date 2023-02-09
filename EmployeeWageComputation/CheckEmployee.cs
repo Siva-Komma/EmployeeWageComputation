89191603df1f2406c15c50cfb2a6cc677c176fa9
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-09 08:36:52 +0530

    Checking Employee Present or Absent

---
+++ EmployeeWageComputation/CheckEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    class CheckEmployee
+    {
+        public static void EmployeeCheck()
+        {
+            int Emp_Present = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == Emp_Present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,6 +11,7 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            CheckEmployee.EmployeeCheck();
             Console.ReadLine();
         }
     }
